cfe0a1967a0b126e085db71334dc18ea0cb480b4
Martin <acostamartin1270@gmail.com>
2024-05-20 00:28:04 -0300

    +tiempo de descubrimiento

--- TP04/HashMap/ServicioDFS.java
+++ TP04/HashMap/ServicioDFS.java
@@ -2,11 +2,13 @@ package HashMap;
 
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedList;
 
 public class ServicioDFS<T> {
 
     private HashMap<Integer, String> colores;
- 
+    private HashMap<Integer, LinkedList<Integer>> tiempos;
+
     public ServicioDFS() {
         this.colores = new HashMap<>();
     }
@@ -17,29 +19,35 @@ public class ServicioDFS<T> {
             Integer vertice = it.next();
             colores.put(vertice,"Blanco");
         }
+        int tiempo = 0;
         Iterator<Integer> it2 = grafo.obtenerVertices();
         while (it2.hasNext()) {
             Integer vertice = it2.next();
             if(colores.get(vertice) == "Blanco"){
-                dfsVisit(grafo,vertice);
+                dfsVisit(grafo,vertice,tiempo);
             }
         }
     }
 
-    private void dfsVisit(Grafo<T> grafo, Integer vertice) {
+    private void dfsVisit(Grafo<T> grafo, Integer vertice, int tiempo) {
         System.out.println(" " + vertice);
         colores.put(vertice,"Amarillo");
+        tiempo++;
+        tiempos.put(vertice, null);
+        tiempos.get(vertice).add(tiempo);
         Iterator<Integer> ady = grafo.obtenerAdyacentes(vertice);
         while(ady.hasNext()){
             Integer v = ady.next();
             if(colores.get(v) == "Blanco"){
-                dfsVisit(grafo, v);
+                dfsVisit(grafo, v, tiempo);
             }
             else if(colores.get(v) == "Amarillo"){
                 System.out.println("Hay ciclo");
             }
-            colores.put(v, "Negro");
         }
+        colores.put(vertice, "Negro");
+        tiempo = tiempo+1;
+        tiempos.get(vertice).add(tiempo);
     }
 
     
